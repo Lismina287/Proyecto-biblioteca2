f314ce18845495c385400596acea7519f71d0802
Azucena <lismina28@gmail.com>
2026-05-03 21:51:08 +0200

    Cambiar de lista a tabla

--- clientes.php
+++ clientes.php
@@ -31,18 +31,32 @@ while(true){
 
         <a href="logout.php">Cerrar sesión</a><br>
         <a href="crear_cliente.php">Crear nuevo cliente</a><br>
+        <a href="info.php">Volver</a><br>
 
-        <ul>
-            <?php foreach ($clientes as $cliente): ?>
-                <li>
-                    <?php echo $cliente->NOMBRE . " " . $cliente->APELLIDOS; ?>
-                    <!-- (<?php echo $cliente->LOCALIDAD; ?>) -->
-
-                    <a href="editar_cliente.php?id=<?php echo $cliente->ID; ?>">Editar | </a>
-                    <a href="borrar_cliente.php?id=<?php echo $cliente->ID; ?>">Borrar</a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+        <table border="1" cellpadding="5">
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Fecha nacimiento</th>
+            <th>Localidad</th>
+            <th></th>
+        </tr>
+        <?php foreach($clientes as $c): ?>
+            <tr>
+                <td><?= $c->ID ?></td>
+                <td><?= $c->NOMBRE ?></td>
+                <td><?= $c->APELLIDOS ?></td>
+                <td><?= $c->FECHA_NACIMIENTO ?></td>
+                <td><?= $c->LOCALIDAD ?></td>
+                <td>
+                    <a href="editar_cliente.php?id=<?= $c->ID ?>">Editar | </a>
+                    <a href="borrar_cliente.php?id=<?= $c->ID ?>">Borrar</a> |
+                    <a href="reserva_cliente.php?id=<?= $c->ID ?>">Reservar</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </table>
     </body>
 </html>
 
