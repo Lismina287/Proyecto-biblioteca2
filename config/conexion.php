985092e62b18a319ee4bdb6586b12e8838d80b34
Azucena <lismina28@gmail.com>
2026-05-03 21:49:47 +0200

    solución de caracteres con ñ o tildes

--- config/conexion.php
+++ config/conexion.php
@@ -6,6 +6,7 @@ $contraseña = "root";
 $nombre_bbdd = "proyecto-biblioteca";
 
 $conexion = new mysqli($servidor, $usuario, $contraseña, $nombre_bbdd);
+$conexion->set_charset("utf8mb4");
 
 if ($conexion->connect_error) {
     echo "Error en la conexión. <br>" . $conexion->connect_error;
